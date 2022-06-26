c59ce4088f2bd26b21fc5bde6e646e768c56213b
jiangyj19 avar <jiangyj19@gmail.com>
2022-06-26 17:53:40 +0000

    database half

--- nftBackend/settings.py
+++ nftBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gqd+@c#_yhw-!x&4!lpojx1a$!#7grk6em6+dcyf-=8@20$^kk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["http://3.92.144.25/"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "signMsg",
+    "queryContract"
 ]
 
 MIDDLEWARE = [
--- queryContract/views.py
+++ queryContract/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 
+network = ''
+contract = ''
 def querytotalSupply(request):
     return HttpResponse("Hello, world. You're at the polls index.")
